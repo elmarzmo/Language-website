960f0b5af6bb66f365c1a4eec0e4c6a51ea47629
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-04-11 03:13:39 -0500

    feat(routes): Define application routes for Home, About, and Contact pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/', redirectTo: '/home', pathMatch: 'full' },
+    { path: '/home', component: Home },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+    
+
+];
+
+
