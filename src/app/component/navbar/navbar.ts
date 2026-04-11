9b0b047baed018644fc4428737fe205f57430ed8
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-04-11 14:08:14 -0500

    feat(navbar): Implement Navbar component with logo, navigation links, and language switcher

--- src/app/component/navbar/navbar.ts
+++ src/app/component/navbar/navbar.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrls: ['./navbar.css']
 })
 export class Navbar {
+  activeLang: string = 'EN';
+  languages: string[] = ['EN', 'FR', 'AR'];
 
+  setLang(lang: string): void {
+    this.activeLang = lang;
+  }
 }
