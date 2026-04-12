107e69e65efaf21a8efec472d8b8861e4bb8e337
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-04-11 19:59:10 -0500

    feat(contact): Implement translation for Contact page content and update imports

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
