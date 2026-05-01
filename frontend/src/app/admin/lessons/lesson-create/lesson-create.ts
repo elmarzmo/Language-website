96399ce88184a90c3508c0634be9263f6d1276a2
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-01 00:50:46 -0500

    feat: implement LessonCreate component for lesson creation functionality

---
+++ frontend/src/app/admin/lessons/lesson-create/lesson-create.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { LessonModule } from '../../../services/lesson.model';
+import { LessonService } from '../../../services/lesson';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lesson-create',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './lesson-create.html',
+  styleUrl: './lesson-create.css',
+})
+export class LessonCreate {
+  
+  lesson: Partial<LessonModule> = {
+    title: '',
+    description: ''
+  };
+
+  constructor(
+    private lessonService: LessonService,
+    private router: Router
+  ) { }
+
+  createLesson() {
+    const role = localStorage.getItem('role' ) || '';
+    if (!this.lesson.title || !this.lesson.description) {
+      alert('Please fill in all fields');
+      return;
+    }
+     this.lessonService.createLesson(this.lesson as LessonModule, role)
+      .subscribe({
+        next: () => {
+          console.log('Lesson created successfully');
+          this.router.navigate(['/admin/lessons']);
+        },
+        error: (err) => {
+          console.error('Error creating lesson', err);
+        }
+      });
+  }
+
+}
