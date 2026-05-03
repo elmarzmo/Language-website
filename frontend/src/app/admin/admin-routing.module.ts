e9ab9a66173581942710eb8a53b4eb0fbd554ec1
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-02 22:51:20 -0500

    feat: update routing for classes to include ClassList and ClassCreate components

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LessonList } from './lessons/lesson-list/lesson-list';
 import { LessonCreate } from './lessons/lesson-create/lesson-create';
 import { ClassCreate } from './classes/class-create/class-create';
+import { ClassList } from './classes/class-list/class-list';
 
 const routes: Routes = [
   {path: 'lessons', component: LessonList},
   {path: 'lessons/create', component: LessonCreate},
-  {path: 'class-sessions', component: ClassCreate}
+
+  {path: 'classes', component: ClassList},
+  {path: 'classes/create', component: ClassCreate}
 ];
 
 @NgModule({
