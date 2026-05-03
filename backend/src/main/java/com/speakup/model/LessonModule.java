a5169d50def3bdf4a002c85ff5023f6beea01813
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-02 23:58:35 -0500

    feat: add  content data

--- backend/src/main/java/com/speakup/model/LessonModule.java
+++ backend/src/main/java/com/speakup/model/LessonModule.java
@@ -1,15 +1,15 @@
 package com.speakup.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "lessons")
 @Data
@@ -22,7 +22,10 @@ public class LessonModule {
 
     private String title;
 
+    private String content;
+
     private String description;
+
 
     private String createdBy; // Teacher ID
 
