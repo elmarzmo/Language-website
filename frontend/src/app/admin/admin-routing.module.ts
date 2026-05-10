f9d75cdbae88d4a90963a321e7cc1f1b76dd9dcb
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-10 01:20:11 -0500

    feat: add route for AdminDashboard component in admin routing module

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -4,8 +4,10 @@ import { LessonList } from './lessons/lesson-list/lesson-list';
 import { LessonCreate } from './lessons/lesson-create/lesson-create';
 import { ClassCreate } from './classes/class-create/class-create';
 import { ClassList } from './classes/class-list/class-list';
+import { AdminDashboard } from './admin-dashboard/adminDashboard' 
 
 const routes: Routes = [
+  {path: '', component: AdminDashboard },
   {path: 'lessons', component: LessonList},
   {path: 'lessons/create', component: LessonCreate},
 
