41ae6cbef14d4d99ac371c3ed5da8382097cd390
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-10 23:03:17 -0500

    feat: update class list UI/UX

--- frontend/src/app/admin/classes/class-list/class-list.ts
+++ frontend/src/app/admin/classes/class-list/class-list.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ClassSession } from '../../models/class-session.model';
 import { ClassSessionService } from '../../service/class-session';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-class-list',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './class-list.html',
   styleUrl: './class-list.css',
 })
 export class ClassList implements OnInit {
 
   classes: ClassSession[] = [];
+  isLoading = true;
 
   constructor(
     private classSessionService: ClassSessionService
@@ -22,12 +24,15 @@ export class ClassList implements OnInit {
   }
 
   loadClassSessions() {
+    this.isLoading = true;
     this.classSessionService.getAllSessions().subscribe({
       next: (data) => {
         this.classes = data;
+        this.isLoading = false;
       },
       error: (err) => {
         console.error('Error fetching class sessions:', err);
+        this.isLoading = false;
       }
     });
   }
