f1587034d6f0f9337824385360a8c1f7bff08fe2
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-10 22:54:26 -0500

    feat: create admin dashboard service

---
+++ frontend/src/app/admin/service/admin-dashboard-service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map, Observable} from 'rxjs'
+import { environment } from '../../../environments/environment';
+import { AdminDashboard } from '../models/admin-dashboard.model';
+
+
+
+export interface ClassSession {
+  id: string;
+  teacherId: string;
+  studentIds: string[];
+  lessonModuleId: string;
+  dateTime: string; // ISO format
+  durationMinutes: number; // in minutes
+  meetingLink: string;
+  status: 'SCHEDULED' | 'ONGOING' | 'COMPLETED' | 'CANCELLED';
+}
+
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class AdminDashboardService {
+
+    private apiUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient) {}
+
+    getAdminDashboard(): Observable<AdminDashboard> {
+        return this.http.get<AdminDashboard>(`${this.apiUrl}/admin`)
+        .pipe(
+          map(data => {
+           
+            if(data.allClasses && data.allClasses.length > 0){
+            data.allClasses.sort((a, b) => new Date(a.dateTime).getTime() - new Date(b.dateTime).getTime());
+            }
+            return data;
+          }
+          )
+        );
+    }
+    
+    
+  
+}
+
