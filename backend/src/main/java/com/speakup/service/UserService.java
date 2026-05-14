85c326cfb70a726b156f20440c5f5166c7bf13ff
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-14 01:48:01 -0500

    feat: add user service to find all student

---
+++ backend/src/main/java/com/speakup/service/UserService.java
@@ -0,0 +1,23 @@
+package com.speakup.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.speakup.model.User;
+import com.speakup.repository.UserRepository;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllStudents(){
+
+        return userRepository.findByRole(User.Role.STUDENT);
+    }
+}
