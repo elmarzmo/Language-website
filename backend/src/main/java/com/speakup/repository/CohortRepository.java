e5a24842999acc41296feb187197bfaa3466eb05
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-17 16:47:32 -0500

    feat: add find by level repository

--- backend/src/main/java/com/speakup/repository/CohortRepository.java
+++ backend/src/main/java/com/speakup/repository/CohortRepository.java
@@ -3,13 +3,17 @@ package com.speakup.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import com.speakup.model.Cohort;
 
-public interface CohortRespository extends MongoRepository<Cohort, String> {
+import com.speakup.model.Cohort;
+import com.speakup.model.CohortLevel;
+
+public interface CohortRepository extends MongoRepository<Cohort, String> {
 
     List<Cohort> findByTeacherId(String teacherId);
 
     List<Cohort> findByActive(boolean active);
+
+    List<Cohort> findByLevel(CohortLevel level);
 
 
 }
