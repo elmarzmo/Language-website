00d829a09260727fa7fb4030eedba2bfbbb3c57c
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-17 13:56:33 -0500

    feat: use validation

--- backend/src/main/java/com/speakup/dto/CreateClassSessionRequest.java
+++ backend/src/main/java/com/speakup/dto/CreateClassSessionRequest.java
@@ -4,16 +4,31 @@ package com.speakup.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateClassSessionRequest {
 
+    @NotBlank(message="Teacher ID is required")
     private String teacherId;
+    
+    @NotEmpty(message="At least one student is required")
     private List<String> studentIds;
+
+    @NotBlank(message="Lesson Module Id is required")
     private String lessonModuleId;
+
+    @NotNull (message="Date and time are required")
     private LocalDateTime dateTime; 
+
+    @Min(value = 60, message= " Duration must be at least 1 hour")
     private int durationMinutes;
+
+    @NotBlank(message="Meeting link is required")
     private String meetingLink;
     
 }
