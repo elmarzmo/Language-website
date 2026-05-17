1be1950863f837aad9a98d617e8aa1b662fae629
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-17 17:10:52 -0500

    feat: add cohort  controller

---
+++ backend/src/main/java/com/speakup/controller/CohortController.java
@@ -0,0 +1,44 @@
+package com.speakup.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.speakup.dto.CohortDTO;
+import com.speakup.dto.CreateCohortRequest;
+import com.speakup.service.CohortService;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/admin/cohorts")
+@CrossOrigin
+public class CohortController {
+    private final CohortService cohortService;
+
+    public CohortController( CohortService cohortService){
+        this.cohortService = cohortService;
+    }
+
+    @PostMapping
+    public CohortDTO createCohort( @Valid @RequestBody CreateCohortRequest request) {
+        return cohortService.createCohortDTO(request);
+    }
+
+    @GetMapping("/teacher/{teacherId}")
+    public List<CohortDTO> getTeacherCohoList( @PathVariable String teacherId){
+        return cohortService.getTeacherCohorts(teacherId);
+    }
+
+    @PostMapping("/{cohortId}/students")
+    public CohortDTO addStudent(@PathVariable String cohortId, String studentId){
+
+        return cohortService.addStudent(cohortId, studentId);
+    }
+}
