89d42a6a16c26cf0be17b4943b6221eef7391938
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-25 19:32:54 -0500

    feat: add teacher controller

---
+++ backend/src/main/java/com/speakup/controller/TeacherController.java
@@ -0,0 +1,31 @@
+package com.speakup.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.speakup.dto.TeacherDashboardDTO;
+import com.speakup.service.TeacherDashboardService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/teacher")
+public class TeacherController {
+
+    private final TeacherDashboardService teacherDashboardService;
+
+    public TeacherController(TeacherDashboardService teacherDashboardService ){
+
+        this.teacherDashboardService = teacherDashboardService;
+    }
+
+    @GetMapping("/teacher/dashboard")
+    public TeacherDashboardDTO getTeacherDashboar(HttpServletRequest request) {
+
+        String teacherId = (String) request.getAttribute("userId");
+
+        return teacherDashboardService.getTeacherDashboard(teacherId);
+    }
+    
+}
