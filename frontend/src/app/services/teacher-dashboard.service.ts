81656a6769318d617ec12ea5c773854ed9d90838
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-25 21:47:53 -0500

    feat: add teacher frontend service

---
+++ frontend/src/app/services/teacher-dashboard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeacherDashboardService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient){}
+
+  getTeacherDashboard(): Observable<any>{
+    return this.http.get(`${this.apiUrl}/teacher/dashboard`);
+  }
+}
