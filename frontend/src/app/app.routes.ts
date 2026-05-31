e47f2d2f3c9d1a69b77d243db17037f33691cebd
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-05-30 23:37:40 -0500

    feat: add teacher routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { About } from './public/about/about';
 import { Contact } from './public/contact/contact';
 import { Home } from './public/home/home';
-import { Dashboard } from './private/dashboard/dashboard';
+import { Dashboard } from './private/student/dashboard/dashboard';
 import { Signin } from './public/signin/signin';
 import { AuthGuard } from './services/auth.guard';
 import { AdminModule } from './admin/admin-module';
-import { LessonView } from './private/lesson-view/lesson-view';
-import { TeacherDashboard } from './private/teacher-dashboard/teacher-dashboard';
+import { LessonView } from './private/student/lesson-view/lesson-view';
+import { TeacherDashboard } from './private/teacher/teacher-dashboard/teacher-dashboard';
+import { TeacherLessonsView } from './private/teacher/teacher-lessons-view/teacher-lessons-view';
+import { TeacherLessonsList } from './private/teacher/teacher-lessons-list/teacher-lessons-list';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -24,7 +26,10 @@ export const routes: Routes = [
 
     { path: 'admin', loadChildren: () => import('./admin/admin-module').then(m => m.AdminModule), canActivate: [AuthGuard] },
     
-    {path: 'teacher/dashboard' , component: TeacherDashboard }
+    {path: 'teacher/dashboard' , component: TeacherDashboard },
+    {path: 'teacher/resources' , component: TeacherLessonsList },
+    {path: 'teacher/resources/:id' , component: TeacherLessonsView }
+
 
 ];
 
