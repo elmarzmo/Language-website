330a1fa2927e79debb46e2a1386a42205e537d53
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-06-02 13:31:45 -0500

    refactor: remove unused findByStudentIdsContaining method from ClassSessionRepository

--- backend/src/main/java/com/speakup/repository/ClassSessionRepository.java
+++ backend/src/main/java/com/speakup/repository/ClassSessionRepository.java
@@ -8,7 +8,7 @@ import com.speakup.model.ClassSession;
 
 public interface ClassSessionRepository extends MongoRepository<ClassSession, String> {
 
-    List<ClassSession> findByStudentIdsContaining(String studentId);
+    
 
     List<ClassSession> findByTeacherId(String teacherId);
 }
