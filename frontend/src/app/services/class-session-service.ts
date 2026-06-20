3b265c47a5c6ad27b9e416db92d96a672779a828
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-06-20 00:25:45 -0500

    feat: add class session service

---
+++ frontend/src/app/services/class-session-service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ClassSession } from '../model/classSession.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClassSessionService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getSessionByTeacher(teacherId: string): Observable<ClassSession[]> {
+    return this.http.get<ClassSession[]>(`${this.apiUrl}/teacher/sessions?teacherId=${teacherId}`);
+
+  }
+
+  updateSession(sessionId: string, payload: Partial<ClassSession>): Observable<ClassSession> {
+    return this.http.put<ClassSession>(`${this.apiUrl}/teacher/sessions/${sessionId}/update`, payload);
+  }
+  
+}
