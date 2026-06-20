c2754d71ff261ef9f5a8060648fe4003110fb4ae
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-06-20 00:15:09 -0500

    feat: implement CohortService with API methods for fetching and updating cohorts

--- frontend/src/app/services/cohort-service.ts
+++ frontend/src/app/services/cohort-service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cohort } from '../model/cohort.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CohortService {
+    private apiUrl = environment.apiUrl;
+
+      constructor(private http: HttpClient) { }
+
+
+      getAllCohorts(): Observable<Cohort[]>{
+          return this.http.get<Cohort[]>(`${this.apiUrl}/admin/cohorts`);
+        }
+
+      updateCohort(cohortId: string, payload: Partial<Cohort>): Observable<Cohort> {
+          return this.http.put<Cohort>(`${this.apiUrl}/admin/cohorts/${cohortId}/update`, payload);
+        }
   
 }
