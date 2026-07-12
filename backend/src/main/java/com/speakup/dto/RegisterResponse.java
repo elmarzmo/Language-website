26c3df6bc3ba4aca7a3def356a1d3ab4f1c46e13
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-07-12 18:34:27 -0500

    feat: add LoginRequest and RegisterRequest DTOs with validation; implement RegisterResponse DTO

---
+++ backend/src/main/java/com/speakup/dto/RegisterResponse.java
@@ -0,0 +1,12 @@
+package com.speakup.dto;
+import lombok.Data;
+
+@Data
+public class RegisterResponse {
+
+    private String id;
+    private String username;
+    private String email;
+    private String role;
+    
+}
