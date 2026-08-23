c31bfe84a130ddcc58fd6bb329dc5532a82229da
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-08-23 00:23:15 -0500

    feat: add StudentOnboardingDTO for student onboarding data transfer

---
+++ backend/src/main/java/com/speakup/dto/StudentOnboardingDTO.java
@@ -0,0 +1,19 @@
+package com.speakup.dto;
+
+import com.speakup.model.StudentOnboarding.EnglishLevel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentOnboardingDTO {
+
+   
+    private EnglishLevel englishLevel;
+    private boolean profileCompleted;
+
+  
+}
