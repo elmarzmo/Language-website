4b0cf68606180247f1cb376a4c038009a0120220
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-08-23 20:56:25 -0500

    feat: create VoucherDTO class for voucher data transfer

---
+++ backend/src/main/java/com/speakup/dto/VoucherDTO.java
@@ -0,0 +1,13 @@
+package com.speakup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class VoucherDTO {
+    private String code;
+}
