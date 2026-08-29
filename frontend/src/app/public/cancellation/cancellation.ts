462bac4a6587694ac25584b25ee5c9e7754502b0
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-08-29 16:15:17 -0500

    feat: add Cancellation component with HTML, CSS, and unit tests

---
+++ frontend/src/app/public/cancellation/cancellation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancellation',
+  imports: [],
+  templateUrl: './cancellation.html',
+  styleUrl: './cancellation.css',
+})
+export class Cancellation {
+
+}
