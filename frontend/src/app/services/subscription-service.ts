eafb7ca4641edec79a03bc68ec9b05779151148f
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-09-10 11:17:11 -0500

    feat: add SubscriptionService with methods to cancel and reactivate subscriptions

---
+++ frontend/src/app/services/subscription-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SubscriptionService {
+  
+  private apiUrl = environment.apiUrl;
+
+
+  constructor(private http: HttpClient) {}
+
+  cancelSubscription(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/student/stripe/cancel`, {});
+  }
+
+  reactivateSubscription(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/student/stripe/reactivate`, {});
+  }
+}
